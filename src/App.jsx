618af3163d058aa6614d392991b4d33c99d74a65
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-03-28 02:46:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Accordian from './components/accordian/Accordian'
+
+const App = () => {
+  return (
+    <>
+    <Accordian/>
+    
+    </>
+  )
+}
+
+export default App
