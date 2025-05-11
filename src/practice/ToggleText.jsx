66ed7449ac90dd9215c007589739fc9362dae133
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-05-11 13:13:47 +0530

    Add files via upload

--- src/practice/ToggleText.jsx
+++ src/practice/ToggleText.jsx
@@ -9,7 +9,9 @@ const ToggleText = () => {
     <div style={styles.container}>
         <h2>Toggle Text Visibility</h2>
         <button onClick={handleToggle}>Toggle Text</button>
-        {isVisible &&  <p style={styles.paragraph}>This is the text that can be toggled.</p>}     
+        {isVisible && <p >This is the text that can be toggled.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eum corporis porro ea, ipsum similique laudantium debitis temporibus voluptatem et. Reiciendis eaque obcaecati aspernatur. Rerum sint illo voluptas aperiam alias eius incidunt sequi! Ratione harum qui eum aspernatur itaque tenetur, sint ad eos consequatur temporibus provident illo inventore pariatur tempore?
+          </p>}     
 
     </div>
   )
